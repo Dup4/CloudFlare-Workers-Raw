a18987d8f86f1a0c87916f5d7dfc7e8349454756
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-12-01 14:30:36 +0800

    feat: url

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 addEventListener('fetch', event => {
-	event.respondWith(handleRequest(event.request))
-});
+    event.respondWith(handleRequest(event.request))
+})
 
 /**
  * Respond to the request
  * @param {Request} request
  */
 async function handleRequest(request) {
-	let url = request.url.substr(8)
-	url = url.substr(url.indexOf('/') + 1)
+    let searchParams = new URLSearchParams(window.location.search)
+    let url = searchParams.get('url')
+    let response = null
+    const host = [window.location.protocol, window.location.hostname].join('//')
 
-	let response
+    if (url) {
+        response = new Response(`${host}?url={url}`, {
+            status: 200,
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        })
+    } else {
+        response = await fetch(url)
+    }
 
-	if (url === '') {
-		response = new Response('Host/{owner}/{repos}/{branch}/{path}', {
-			status: 200,
-			headers: {
-				'Content-Type': 'text/html',
-			},
-		})
-	} else {
-		// url = 'https://raw.githubusercontent.com/' + url;
-		response = await fetch(url);
-	}
+    let myHeaders = new Headers(response.headers)
+    myHeaders.set('Access-Control-Allow-Origin', '*')
 
-	let myHeaders = new Headers(response.headers);
-	myHeaders.set('Access-Control-Allow-Origin', '*');
+    let ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase(),
+        ct = null
+    switch (ext) {
+        case 'svg':
+            ct = 'image/svg+xml'
+            break
+        case 'js':
+            ct = 'application/javascript'
+            break
+        case 'css':
+            ct = 'text/css'
+            break
+        case 'pdf':
+            ct = 'application/pdf'
+            break
+    }
 
-	let ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase(),
-		ct = null;
-	switch (ext) {
-		case 'svg':
-			ct = 'image/svg+xml';
-			break;
-		case 'js':
-			ct = 'application/javascript';
-			break;
-		case 'css':
-			ct = 'text/css';
-			break;
-		case 'pdf':
-			ct = 'application/pdf';
-			break;
-	}
+    if (ct) {
+        myHeaders.set('Content-Type', ct)
+    }
 
-	if (ct) {
-		myHeaders.set('Content-Type', ct);
-	}
-
-	return new Response(response.body, {
-		status: response.status,
-		headers: myHeaders,
-	});
+    return new Response(response.body, {
+        status: response.status,
+        headers: myHeaders,
+    })
 }
